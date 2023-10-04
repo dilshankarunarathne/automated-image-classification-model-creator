e91780ab8b075bff937ae3f0862ea3995a7e495d
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-04 19:37:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import image_miner
+from image_miner import downloader
 
 query = input("Enter the search query: ")
 num_images = int(input("Enter the number of images: "))
 
-image_miner.downloader.download(
+downloader.download(
     query,
     limit=num_images,
     output_dir='dataset',
