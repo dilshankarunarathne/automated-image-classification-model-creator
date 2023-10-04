edd3ef13c03ef6ecd0b05283fd39db938956ce2f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-04 19:34:28 +0530

    Update bing.py

--- image-miner/bing.py
+++ image-miner/bing.py
@@ -12,7 +12,7 @@ Author: Guru Prasad (g.gaurav541@gmail.com)
 
 
 class Bing:
-    def __init__(self, query, limit, output_dir, adult, timeout,  filter='', verbose=True):
+    def __init__(self, query, limit, output_dir, adult, timeout, filter='', verbose=True):
         self.download_count = 0
         self.query = query
         self.output_dir = output_dir
@@ -28,30 +28,28 @@ class Bing:
 
         # self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'}
         self.page_counter = 0
-        self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' 
-      'AppleWebKit/537.11 (KHTML, like Gecko) '
-      'Chrome/23.0.1271.64 Safari/537.11',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-      'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-      'Accept-Encoding': 'none',
-      'Accept-Language': 'en-US,en;q=0.8',
-      'Connection': 'keep-alive'}
-
+        self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
+                                      'AppleWebKit/537.11 (KHTML, like Gecko) '
+                                      'Chrome/23.0.1271.64 Safari/537.11',
+                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+                        'Accept-Encoding': 'none',
+                        'Accept-Language': 'en-US,en;q=0.8',
+                        'Connection': 'keep-alive'}
 
     def get_filter(self, shorthand):
-            if shorthand == "line" or shorthand == "linedrawing":
-                return "+filterui:photo-linedrawing"
-            elif shorthand == "photo":
-                return "+filterui:photo-photo"
-            elif shorthand == "clipart":
-                return "+filterui:photo-clipart"
-            elif shorthand == "gif" or shorthand == "animatedgif":
-                return "+filterui:photo-animatedgif"
-            elif shorthand == "transparent":
-                return "+filterui:photo-transparent"
-            else:
-                return ""
-
+        if shorthand == "line" or shorthand == "linedrawing":
+            return "+filterui:photo-linedrawing"
+        elif shorthand == "photo":
+            return "+filterui:photo-photo"
+        elif shorthand == "clipart":
+            return "+filterui:photo-clipart"
+        elif shorthand == "gif" or shorthand == "animatedgif":
+            return "+filterui:photo-animatedgif"
+        elif shorthand == "transparent":
+            return "+filterui:photo-transparent"
+        else:
+            return ""
 
     def save_image(self, link, file_path):
         request = urllib.request.Request(link, None, self.headers)
@@ -62,7 +60,6 @@ class Bing:
         with open(str(file_path), 'wb') as f:
             f.write(image)
 
-    
     def download_image(self, link):
         self.download_count += 1
         # Get the image link
@@ -72,11 +69,11 @@ class Bing:
             file_type = filename.split(".")[-1]
             if file_type.lower() not in ["jpe", "jpeg", "jfif", "exif", "tiff", "gif", "bmp", "png", "webp", "jpg"]:
                 file_type = "jpg"
-                
+
             if self.verbose:
                 # Download the image
                 print("[%] Downloading Image #{} from {}".format(self.download_count, link))
-                
+
             self.save_image(link, self.output_dir.joinpath("Image_{}.{}".format(
                 str(self.download_count), file_type)))
             if self.verbose:
@@ -86,7 +83,6 @@ class Bing:
             self.download_count -= 1
             print("[!] Issue getting: {}\n[!] Error:: {}".format(link, e))
 
-    
     def run(self):
         while self.download_count < self.limit:
             if self.verbose:
@@ -94,11 +90,12 @@ class Bing:
             # Parse the page source and download pics
             request_url = 'https://www.bing.com/images/async?q=' + urllib.parse.quote_plus(self.query) \
                           + '&first=' + str(self.page_counter) + '&count=' + str(self.limit) \
-                          + '&adlt=' + self.adult + '&qft=' + ('' if self.filter is None else self.get_filter(self.filter))
+                          + '&adlt=' + self.adult + '&qft=' + (
+                              '' if self.filter is None else self.get_filter(self.filter))
             request = urllib.request.Request(request_url, None, headers=self.headers)
             response = urllib.request.urlopen(request)
             html = response.read().decode('utf8')
-            if html ==  "":
+            if html == "":
                 print("[%] No more images are available")
                 break
             links = re.findall('murl&quot;:&quot;(.*?)&quot;', html)
