6beb9040d8da0dfc6d4a5bca2ba7622eaa1cc2cd
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-04 19:36:15 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+query = input("Enter the search query: ")
+num_images = int(input("Enter the number of images: "))
+
+downloader.download(
+    query,
+    limit=num_images,
+    output_dir='dataset',
+    adult_filter_off=False,
+    force_replace=False,
+    timeout=60,
+    verbose=True
+)
