3aa0fdefc99e85c42622382d15115dd0f3cf803a
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-07 19:16:26 +0530

    Update creator.py

--- creator.py
+++ creator.py
@@ -67,12 +67,11 @@ if __name__ == '__main__':
     # create train dataset
     print("train dataset seperation started")
     for cl in classes:
-        
-    for (root, dirs, files) in os.walk('dataset', topdown=True):
-        for file in files:
-            print("working on ", root, " directory dataset")
-            source = root + "/" + file
-            destination = ""
+        for (root, dirs, files) in os.walk('dataset', topdown=True):
+            for file in files:
+                print("working on ", root, " directory dataset")
+                source = root + "/" + file
+                destination = ""
 
 
     # create dataset
