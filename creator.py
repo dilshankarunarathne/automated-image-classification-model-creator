4559bb52b98568ccac1e5f5e476fba6cf5481387
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-07 18:49:07 +0530

    Update creator.py

--- creator.py
+++ creator.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
+
 path_to_plants = "dataset"
 plant_classes = os.listdir(path_to_plants)
 train_dir = 'dataset/plants'
