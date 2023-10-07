d0186ac6e9775838a4dc656d841a26006dbe437e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-07 18:51:03 +0530

    Update creator.py

--- creator.py
+++ creator.py
@@ -1,9 +1,8 @@
 import os
-import tensorflow as tf
+
 from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.models import Sequential
 
 path_to_plants = "dataset"
 plant_classes = os.listdir(path_to_plants)
