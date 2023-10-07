07f4c3d0db555371145c066ac1f1705110f4d79b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-07 18:48:46 +0530

    Update creator.py

--- creator.py
+++ creator.py
@@ -9,7 +9,7 @@ train_dir = 'dataset/plants'
 
 # Create data generators
 train_datagen = keras.preprocessing.image.ImageDataGenerator(
-    rescale=1./255,
+    rescale=1. / 255,
 )
 
 # Define batch size and image dimensions
@@ -17,13 +17,12 @@ batch_size = 50
 img_height = 150
 img_width = 150
 
-#Load and prepare training data
+# Load and prepare training data
 train_data_gen = train_datagen.flow_from_directory(
     train_dir,
-    target_size = (img_height, img_width),
-    batch_size = batch_size,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
 
-    class_mode = 'categorical',       # Use 'categorical for multi-class classification shuffle=True
-    shuffle = True
+    class_mode='categorical',  # Use 'categorical for multi-class classification shuffle=True
+    shuffle=True
 )
-
