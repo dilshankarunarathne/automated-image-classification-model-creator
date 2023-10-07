4377b84cb96d688340671969ec6c45554074a2d9
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-07 18:55:10 +0530

    Update creator.py

--- creator.py
+++ creator.py
@@ -67,6 +67,6 @@ model.save("models/classifier_sequential.h5")
 if __name__ == '__main__':
     classes = input("Enter the image classes (seperated by spaces) that you'd like to classify: ").split(" ")
 
-    
+    # create dataset
     for cls in classes:
 
