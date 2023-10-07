9f743e7a1b3e9f1ea9a6ac4a5ae4598c46a4c8c3
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-07 18:47:45 +0530

    Update creator.py

--- creator.py
+++ creator.py
@@ -1,4 +1,7 @@
 import os
+import tensorflow as tf        # tensorflow library
+from tensorflow import keras   # keras hight level natural network API
+from tensorflow.keras import layers  # type of layers
 
 path_to_plants = "dataset"
 plant_classes = os.listdir(path_to_plants)
